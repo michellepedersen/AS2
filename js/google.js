83410dc4a6ec27fdc4db00b0f82e1b35a1151a7d
Michelle Pedersen <MichellePedersen@myop.ac.nz>
2023-05-24 20:09:14 +1200

    2

---
+++ js/google.js
@@ -0,0 +1,20 @@
+/* Global Site Tag (gtag.js) - Google Analytics */
+src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'GA_TRACKING_ID');
+
+/*
+        <!-- Global Site Tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'GA_TRACKING_ID');
+        </script>
+        
+        end of the </head>
+*/
